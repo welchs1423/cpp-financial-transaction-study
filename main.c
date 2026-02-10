9ea7c8a7c4fb20255f57dfb85f35f139020c7ddf
welchs1423 <welchs1423@naver.com>
2026-02-11 03:29:44 +0900

    feat: 입출금 함수 구현 및 잔액 검증 로직 추가

--- main.c
+++ main.c
@@ -7,21 +7,28 @@ typedef struct {
     long    balance;        // 잔고 (단위: 원)   
 }   Account;
 
+// 잔고에 금액을 더합니다.
+void deposit(Account *acc, long amount){
+    acc->balance += amount;
+    printf("[입금 완료] %ld원 (현재 잔고: %ld원)\n", amount, acc->balance);
+}
+
+int withdraw(Account *acc, long amount){
+    if (acc->balance < amount){
+        printf("[출금 실패] 잔액이 부족합니다. (현재 잔고: %ld원)\n", acc->balance);
+        return -1;  // 실패 시 -1 반환
+    }
+    acc->balance -= amount;
+    printf("[출금 완료] %ld원 (현재 잔고: %ld원)\n", amount, acc->balance);
+    return 0;
+}
+
 int main(){
-    Account my_acc;
-
-    strcpy(my_acc.acc_no, "110-123-456789");
-    strcpy(my_acc.user_name, "Park Developer");
-    my_acc.balance = 5000000;
-
-    // 2. 결과 출력
-    printf("==============================\n");
-    printf("   [C-STOCK] 계좌 정보 확인   \n");
-    printf("==============================\n");
-    printf(" 계좌번호 : %s\n", my_acc.acc_no);
-    printf(" 고 객 명 : %s\n", my_acc.user_name);
-    printf(" 현재잔고 : %ld 원\n", my_acc.balance);
-    printf("==============================\n");
+    Account my_acc = {"110-123-456789", "Park Developer", 5000000};
+    
+    deposit(&my_acc,1000000);   // 100만원 입금
+    deposit(&my_acc, 7000000);  // 700만원 출금 시도 (부족 해야함)
+    withdraw(&my_acc, 3000000);  // 300만원 출금 성공
 
     return 0;
 }
