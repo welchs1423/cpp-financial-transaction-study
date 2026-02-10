e48b609a6539854e0324fe7c90e0dc6c7501112e
welchs1423 <welchs1423@naver.com>
2026-02-11 03:52:22 +0900

    feat: 파일 입출력을 통한 데이터 영속성 구현 (account.txt)

--- main.c
+++ main.c
@@ -7,28 +7,56 @@ typedef struct {
     long    balance;        // 잔고 (단위: 원)   
 }   Account;
 
-// 잔고에 금액을 더합니다.
-void deposit(Account *acc, long amount){
-    acc->balance += amount;
-    printf("[입금 완료] %ld원 (현재 잔고: %ld원)\n", amount, acc->balance);
+// 현재 계좌 정보를 파일에 저장
+void save_to_file(Account *a) {
+    FILE *fp = fopen("account.txt", "w");   // 쓰기 모드로 파일 열기
+    if (fp == NULL){
+        printf("파일을 열수 없습니다!\n");
+        return;
+    }
+    fprintf(fp, "%s %s %ld", a->acc_no, a->user_name, a->balance);
+    fclose(fp);
+    printf(">> 데이터가 account.txt에 저장되었습니다. \n");
 }
 
-int withdraw(Account *acc, long amount){
-    if (acc->balance < amount){
-        printf("[출금 실패] 잔액이 부족합니다. (현재 잔고: %ld원)\n", acc->balance);
-        return -1;  // 실패 시 -1 반환
+void load_from_file(Account *a){
+    FILE *fp = fopen("account.txt", "r");
+    if(fp == NULL){
+        printf(">> 기존 데이터가 없습니다. 기본 정보를 생성합니다. \n");
+        return;
     }
-    acc->balance -= amount;
-    printf("[출금 완료] %ld원 (현재 잔고: %ld원)\n", amount, acc->balance);
-    return 0;
+    fscanf(fp, "%s %s %ld", a->acc_no, a->user_name, &a->balance);
+    fclose(fp);
+    printf(">> 데이터를 파일에서 성공적으로 불러왔습니다. \n");
 }
 
 int main(){
-    Account my_acc = {"110-123-456789", "Park Developer", 5000000};
-    
-    deposit(&my_acc,1000000);   // 100만원 입금
-    deposit(&my_acc, 7000000);  // 700만원 출금 시도 (부족 해야함)
-    withdraw(&my_acc, 3000000);  // 300만원 출금 성공
+    Account my_acc = {"110-123-456", "Park_Dev", 5000000};
+
+    load_from_file(&my_acc);
+    printf("\n--- 현재 계좌 정보 ---\n");
+    printf("계좌번호: %s | 고객명: %s | 잔액: %ld원\n\n", my_acc.acc_no,my_acc.user_name,my_acc.balance);
+
+    int choice;
+    long amount;
+    printf("1.입금 2.출금 (번호 선택): ");
+    scanf("%d", &choice);
+    printf("금액 입력: ");
+    scanf("%ld", &amount);
+
+    if(choice == 1){
+        my_acc.balance += amount;
+        printf("[입금 완료] 현재 잔액: %ld원\n", my_acc.balance);
+    } else if(choice == 2){
+        if(my_acc.balance < amount){
+            printf("[실패] 잔액이 부족합니다!\n");
+        } else {
+            my_acc.balance -= amount;
+            printf("[출금 완료] 현재 잔액: %ld원\n", my_acc.balance);
+        }
+    }
+
+    save_to_file(&my_acc);
 
     return 0;
 }
